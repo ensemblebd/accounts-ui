86a7099e0a8e4ee8f0e31d5d5702bc9a852caa4d
barry durand <barrydurand87@gmail.com>
2020-10-26 22:39:45 -0500

    updated to latest meteor, w/o integrity check

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('1.11.1');
   api.use('ecmascript');
   api.use('accounts-base');
   api.use('check');
